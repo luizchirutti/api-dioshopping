4bc621088ed1848430b8746c2f99bd6d01c08446
Luiz Chirutti <luizchirutti@gmail.com>
2022-04-04 21:03:32 -0300

    config adjust

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router)
 
-app.listen(5000, () => {
-    console.log('Server on port: 5000')
+app.listen(3333, () => {
+    console.log('Server on port: 3333')
 })
